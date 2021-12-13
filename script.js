bb1a1c1f2d8d04b0b436fbc18dc2e905f85fc1bc
RayFisher-24 <arijitmondal0024@gmail.com>
2021-12-13 18:58:54 +0530

    Covid-19 Tracker

---
+++ script.js
@@ -0,0 +1,32 @@
+
+let n = window.prompt("Enter the Country Number");
+
+fetch('https://api.covid19api.com/summary')
+    .then(res => res.json())
+    .then(death2 => {
+        let countryName = death2.Countries[n].Country;
+        let newCase = death2.Countries[n].NewConfirmed;
+        let totalCase = death2.Countries[n].TotalConfirmed;
+        let newDeath = death2.Countries[n].NewDeaths;
+        let totalDeath = death2.Countries[n].TotalDeaths;
+
+        let name=document.getElementById('country');
+        let newC=document.getElementById('new-case');
+        let tC=document.getElementById('total-case');
+        let newD=document.getElementById('new-death');
+        let tD=document.getElementById('total-death');
+
+        name.innerHTML = countryName;
+        newC.innerHTML = newCase;
+        tC.innerHTML = totalCase;
+        newD.innerHTML = newDeath;
+        tD.innerHTML = totalDeath;
+
+        /* 
+        console.log("Country Name: "+countryName);
+        console.log("New Case: "+newCase);
+        console.log("Total Case: "+totalCase);
+        console.log("New Death: "+newDeath);
+        console.log("Total Death: "+totalDeath); */
+    })
+    .catch("Error")
